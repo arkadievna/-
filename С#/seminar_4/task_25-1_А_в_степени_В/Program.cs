6e237d1decde682aeef964c4f335168449560c80
arcadievna <reshetnikovaya@mail.ru>
2023-09-26 02:22:02 +0500

    Добавила решения задач в четвёртый семинар

---
+++ С#/seminar_4/task_25-1_А_в_степени_В/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Math.Pow(x, y) - это функция, которая возводит число x в степень y и возвращает результат. 
+// В данном случае Math.Pow(3, 5) вернет 243 (3 в пятой степени).
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите число A: ");
+        double A = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        double result = Power(A, B);
+        Console.WriteLine("A в степени B = " + result);
+    }
+
+    public static double Power(double A, int B)
+    {
+        if (B == 0)
+        {
+            return 1;
+        }
+
+        double result = A;
+        int exponent = Math.Abs(B) - 1;
+
+        while (exponent > 0)
+        {
+            result *= A;
+            exponent--;
+        }
+
+        if (B < 0)
+        {
+            result = 1 / result;
+        }
+
+        return result;
+    }
+}
---
+++ С#/seminar_4/task_27-2_ оценка_времени_алгоритма/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // ввод числа
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()?? "0");
+
+        // вычисление суммы цифр в числе
+        int sum = 0;
+        int tempNumber = number;
+        while (tempNumber > 0)
+        {
+            sum += tempNumber % 10;
+            tempNumber /= 10;
+        }
+
+        // вывод суммы цифр на экран
+        Console.WriteLine("Сумма цифр в числе: " + sum);
+
+        // оценка времени 
+        string timeMessage = "Время выполнения алгоритма: ";
+        double startTime = DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond;
+        string endTimeString = string.Empty;
+        double endTime = DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond;
+        double elapsedTime = endTime - startTime;
+        timeMessage += elapsedTime.ToString("F6") + " сек.";
+        Console.WriteLine(timeMessage);
+    }
+}
+
